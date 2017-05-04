dcde5edb99ef43bcd29025fb60bed97748ad05c3
BAAMOSK <bunth667@gmail.com>
2017-05-04 12:12:26 -0400

    fixed tests

--- server.js
+++ server.js
@@ -19,123 +19,123 @@ app.use(bodyParser.json());
 mongoose.Promise = global.Promise;
 
 app.get('/posts', (req, res) => {
-	BlogPost
+  BlogPost
 		.find()
 		.exec()
 		.then(posts => {
-			res.json(posts.map(post => post.apiRepr()));
-		})
+  res.json(posts.map(post => post.apiRepr()));
+})
 		.catch(err => {
-			console.error(err);
-			res.status(500).json({
-				error: 'something went terribly wrong'
-			});
-		});
+  console.error(err);
+  res.status(500).json({
+    error: 'something went terribly wrong'
+  });
+});
 });
 
 app.get('/posts/:id', (req, res) => {
-	BlogPost
+  BlogPost
 		.findById(req.params.id)
 		.exec()
 		.then(post => res.json(post.apiRepr()))
 		.catch(err => {
-			console.error(err);
-			res.status(500).json({
-				error: 'something went horribly awry'
-			});
-		});
+  console.error(err);
+  res.status(500).json({
+    error: 'something went horribly awry'
+  });
+});
 });
 
 app.post('/posts', (req, res) => {
-	const requiredFields = ['title', 'content', 'author'];
-	for (let i = 0; i < requiredFields.length; i++) {
-		const field = requiredFields[i];
-		if (!(field in req.body)) {
-			const message = `Missing \`${field}\` in request body`;
-			console.error(message);
-			return res.status(400).send(message);
-		}
-	}
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
 
-	BlogPost
+  BlogPost
 		.create({
-			title: req.body.title,
-			content: req.body.content,
-			author: req.body.author
-		})
+  title: req.body.title,
+  content: req.body.content,
+  author: req.body.author
+})
 		.then(blogPost => res.status(201).json(blogPost.apiRepr()))
 		.catch(err => {
-			console.error(err);
-			res.status(500).json({
-				error: 'Something went wrong'
-			});
-		});
+  console.error(err);
+  res.status(500).json({
+    error: 'Something went wrong'
+  });
+});
 
 });
 
 
 app.delete('/posts/:id', (req, res) => {
-	BlogPost
+  BlogPost
 		.findByIdAndRemove(req.params.id)
 		.exec()
 		.then(() => {
-			res.status(204).json({
-				message: 'success'
-			});
-		})
+  res.status(204).json({
+    message: 'success'
+  });
+})
 		.catch(err => {
-			console.error(err);
-			res.status(500).json({
-				error: 'something went terribly wrong'
-			});
-		});
+  console.error(err);
+  res.status(500).json({
+    error: 'something went terribly wrong'
+  });
+});
 });
 
 
 app.put('/posts/:id', (req, res) => {
-	if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-		res.status(400).json({
-			error: 'Request path id and request body id values must match'
-		});
-	}
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
 
-	const updated = {};
-	const updateableFields = ['title', 'content', 'author'];
-	updateableFields.forEach(field => {
-		if (field in req.body) {
-			updated[field] = req.body[field];
-		}
-	});
+  const updated = {};
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
 
-	BlogPost
+  BlogPost
 		.findByIdAndUpdate(req.params.id, {
-			$set: updated
-		}, {
-			new: true
-		})
+  $set: updated
+}, {
+  new: true
+})
 		.exec()
 		.then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
-		.catch(err => res.status(500).json({
-			message: 'Something went wrong'
-		}));
+		.catch(() => res.status(500).json({
+  message: 'Something went wrong'
+}));
 });
 
 
 app.delete('/:id', (req, res) => {
-	BlogPosts
+  BlogPost
 		.findByIdAndRemove(req.params.id)
 		.exec()
 		.then(() => {
-			console.log(`Deleted blog post with id \`${req.params.ID}\``);
-			res.status(204).end();
-		});
+  console.log(`Deleted blog post with id \`${req.params.ID}\``);
+  res.status(204).end();
+});
 });
 
 
-app.use('*', function(req, res) {
-	res.status(404).json({
-		message: 'Not Found'
-	});
+app.use('*', function (req, res) {
+  res.status(404).json({
+    message: 'Not Found'
+  });
 });
 
 // closeServer needs access to a server object, but that only
@@ -145,47 +145,47 @@ let server;
 
 // this function connects to our database, then starts the server
 function runServer(databaseUrl = DATABASE_URL, port = PORT) {
-	return new Promise((resolve, reject) => {
-		mongoose.connect(databaseUrl, err => {
-			if (err) {
-				return reject(err);
-			}
-			server = app.listen(port, () => {
-					console.log(`Your app is listening on port ${port}`);
-					resolve();
-				})
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
 				.on('error', err => {
-					mongoose.disconnect();
-					reject(err);
-				});
-		});
-	});
+  mongoose.disconnect();
+  reject(err);
+});
+    });
+  });
 }
 
 // this function closes the server, and returns a promise. we'll
 // use it in our integration tests later.
 function closeServer() {
-	return mongoose.disconnect().then(() => {
-		return new Promise((resolve, reject) => {
-			console.log('Closing server');
-			server.close(err => {
-				if (err) {
-					return reject(err);
-				}
-				resolve();
-			});
-		});
-	});
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
 }
 
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-	runServer().catch(err => console.error(err));
-};
+  runServer().catch(err => console.error(err));
+}
 
 module.exports = {
-	runServer,
-	app,
-	closeServer
+  runServer,
+  app,
+  closeServer
 };
--- test/unit-tests.js
+++ test/unit-tests.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose');
 chai.should();
 const {
 	runServer,
-	app,
-	closeServer
+  app,
+  closeServer
 } = require('../server');
 const {
 	BlogPost
@@ -43,122 +43,122 @@ function tearDownDb() {
   return mongoose.connection.dropDatabase();
 }
 
-describe('blog posts testing suite', function() {
-  before(function() {
+describe('blog posts testing suite', function () {
+  before(function () {
     return runServer(DATABASE_URL);
   });
-  beforeEach(function() {
+  beforeEach(function () {
     return seedData();
   });
-  afterEach(function() {
+  afterEach(function () {
     return tearDownDb();
   });
-  after(function() {
+  after(function () {
     return closeServer();
   });
 
-  describe('GET endpoints', function() {
-    it('should return all the blog posts', function() {
+  describe('GET endpoints', function () {
+    it('should return all the blog posts', function () {
       let res;
       return chai.request(app)
         .get('/posts')
-        .then(function(_res) {
+        .then(function (_res) {
           res = _res;
           res.should.have.status(200);
           res.body.should.have.length.of.at.least(1);
           return BlogPost.count();
         })
-        .then(function(count) {
+        .then(function (count) {
           res.body.should.have.length.of(count);
         });
     });
-    it('should return blog posts with the right fields', function() {
+    it('should return blog posts with the right fields', function () {
       let resPost;
       return chai.request(app)
-      .get('/posts')
-      .then(function(res) {
-        res.should.be.json;
-        res.body.should.be.a('array');
-        res.body.forEach(function(post) {
-          post.should.be.a('object');
-          post.should.include.keys('id', 'author', 'content', 'created', 'title');
+        .get('/posts')
+        .then(function (res) {
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.forEach(function (post) {
+            post.should.be.a('object');
+            post.should.include.keys('id', 'author', 'content', 'created', 'title');
+          });
+          resPost = res.body[0];
+          return BlogPost.findById(resPost.id);
+        })
+        .then(function (post) {
+          // console.log(resPost);
+          // console.log(resPost.author);
+          // console.log(post.author);
+          resPost.id.should.equal(post.id);
+          resPost.title.should.equal(post.title);
+          resPost.author.should.equal(post.authorName);
+          resPost.content.should.equal(post.content);
         });
-        resPost = res.body[0];
-        return BlogPost.findById(resPost.id);
-      })
-			.then(function(post) {
-				// console.log(resPost);
-				// console.log(resPost.author);
-				// console.log(post.author);
-  resPost.id.should.equal(post.id);
-  resPost.title.should.equal(post.title);
-  resPost.author.should.equal(post.authorName);
-  resPost.content.should.equal(post.content);
-});
     });
   });
 
-  describe('POST endpoints', function() {
+  describe('POST endpoints', function () {
     it('should add a new blog post', function () {
       const newPost = generateBlogPosts();
       return chai.request(app)
-			.post('/posts')
-			.send(newPost)
-			.then(function(res) {
-  res.should.have.status(201);
-  res.body.should.be.a('object');
-  res.should.be.json;
-  res.body.should.include.keys('id', 'author', 'content', 'created', 'title');
-  res.body.should.not.be.null;
-  res.body.id.should.equal(res.body.id);
-});
-    });
+        .post('/posts')
+        .send(newPost)
+        .then(function (res) {
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.should.be.json;
+          res.body.should.include.keys('id', 'author', 'content', 'created', 'title');
+          res.body.should.not.be.null;
+          res.body.id.should.equal(res.body.id);
+        });
+    });//need to double check in database
   });
 
-  describe('PUT endpoints', function() {
-    it('should update fields you send over', function() {
+  describe('PUT endpoints', function () {
+    it('should update fields you send over', function () {
       const updateData = {
         title: faker.name.title()
       };
       const oldItemAuthor = {};
       return BlogPost
-				.findOne()
-				.exec()
-				.then(function(post) {
-  updateData.id = post.id;
-  oldItemAuthor.author = post.authorName;
-  return chai.request(app)
-					.put(`/posts/${post.id}`)
-					.send(updateData);
-})
-				.then(function(res) {
-  res.should.have.status(201);
-  console.log(oldItemAuthor);
+        .findOne()
+        .exec()
+        .then(function (post) {
+          updateData.id = post.id;
+          oldItemAuthor.author = post.authorName;
+          return chai.request(app)
+            .put(`/posts/${post.id}`)
+            .send(updateData);
+        })
+        .then(function (res) {
+          res.should.have.status(201);
+          console.log(oldItemAuthor);
 
-  return BlogPost.findById(updateData.id).exec();
-})
-				.then(function(post) {
-  post.authorName.should.equal(oldItemAuthor.author);
-  post.title.should.equal(updateData.title);
-});
+          return BlogPost.findById(updateData.id).exec();
+        })
+        .then(function (post) {
+          post.authorName.should.equal(oldItemAuthor.author);
+          post.title.should.equal(updateData.title);
+        });
     });
   });
 
-  describe('DELETE endpoints', function() {
-    it('it should delete post', function() {
+  describe('DELETE endpoints', function () {
+    it('it should delete post', function () {
       let post;
       return BlogPost
-			.findOne()
-			.exec()
-			.then(function(_post) {
-  post = _post;
-  return chai.request(app)				
-				.delete(`/posts/${post.id}`);								
-})
-			.then(function(res) {
-  res.should.have.status(204);
-  return BlogPost.findById(post.id);
-});
+        .findOne()
+        .exec()
+        .then(function (_post) {
+          post = _post;
+          return chai.request(app)
+            .delete(`/posts/${post.id}`);
+        })
+        .then(function (res) {
+          res.should.have.status(204);
+          return BlogPost.findById(post.id);
+        });//check for length of 0;
     });
   });
 
